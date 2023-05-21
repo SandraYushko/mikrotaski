f1a3c7ee3343fcde91227904b4113a815696c53e
SandraYushko <shyria6@gmail.com>
2023-05-21 23:54:07 +0300

    Mikrotaski 04.01 Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent} from 'react';
 import './App.css';
 
 function App() {
-    const myFirstSubscriber = () => {
-        console.log("Hello i'm Vasya")
-    }
-    const mySecondSubscriber = () => {
-        console.log("Hello i'm Ivan")
-    }
+    // const myFirstSubscriber = (e:MouseEvent<HTMLButtonElement>) => {
+    //     console.log("Hello i'm Vasya")
+    // }
+    // const mySecondSubscriber = (e:MouseEvent<HTMLButtonElement) => {
+    //     console.log("Hello i'm Ivan")
+    // }
+
+    const onClickHandler=(name:string)=>{console.log(name)}
     return (
         <div className="App">
-            <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>
-            <button onClick={mySecondSubscriber}>MyYouTubeChanel-2</button>
+            <button onClick={()=>{onClickHandler("Vasya")}}>MyYouTubeChanel-1</button>
+            <button onClick={()=>{onClickHandler("Igor")}}>MyYouTubeChanel-2</button>
         </div>
     )
 }
