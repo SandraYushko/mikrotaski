2bca97c0400d0065bc9bffcc24965eb51d36224c
SandraYushko <shyria6@gmail.com>
2023-05-23 00:35:25 +0300

    Mikrotaski 04.03 Button

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import './App.css';
 import {Button} from "./components/Button";
 
 function App() {
-    const button1Foo = () => {
-console.log("i'm Vasua")
+    const button1Foo = (subscriber:string, age:number) => {
+        console.log(subscriber,age)
     }
-    const button2Foo = () => {
-        console.log("i'm Ivan")
+    const button2Foo = (subscriber:string) => {
+        console.log(subscriber)
+    }
+    const button3Foo = () => {
+        console.log("i'm stupid button")
     }
     return (
         <div className="App">
-            <Button name={"MyYouTubeChanel-1"} callBack={button1Foo}/>
-            <Button name={"MyYouTubeChanel-2"} callBack={button2Foo}/>
+            <Button name={"MyYouTubeChanel-1"} callBack={()=>button1Foo("i'm Ivan",22)}/>
+            <Button name={"MyYouTubeChanel-2"} callBack={()=>button2Foo("i'm Vasya")}/>
+            <Button name={"MyYouTubeChanel-3"} callBack={button3Foo}/>
         </div>
     )
 }
